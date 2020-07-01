e2c9d7ea2d1f10f5454ef6c89abacc23f36aaeb2
Nate <lao.nathan@yahoo.com>
2020-06-30 23:54:51 -0400

    gitignore

---
+++ matrix.h
@@ -0,0 +1,27 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <vector>
+
+
+template <class T> class Matrix
+{
+public:
+	Matrix();
+	~Matrix();
+private:
+};
+
+
+template <class T> Matrix<T>::Matrix()
+{
+	
+}
+
+template <class T> Matrix<T>::~Matrix()
+{
+	
+}
+
+
+#endif
